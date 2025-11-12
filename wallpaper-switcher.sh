9fad3de2aa6f403779fcc8d4f7cd144ad2267f04
jona <137075920+Jona1302@users.noreply.github.com>
2025-11-12 10:29:24 +0000

    Add files via upload

---
+++ wallpaper-switcher.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+WALLPAPER_DIR="$HOME/Pictures/Wallpaper/"
+
+choices=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" -o -iname "*.webp" \) | sort)
+
+chosen=$(echo "$choices" | rofi -dmenu -i -p "Select a Wallpaper:")
+
+[ -z "$chosen" ] && exit
+
+swww img "$chosen" --transition-type any --transition-fps 60 --transition-duration 1
+
+notify-send "Wallpaper switched" "$(basename "$chosen")"
